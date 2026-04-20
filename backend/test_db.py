303c9a3204997feeefa39f1d5b456a9c8076cd61
cnm5084 <cnm5084@gmail.com>
2026-04-20 16:45:40 +0800

    dbConnection

---
+++ backend/test_db.py
@@ -0,0 +1,13 @@
+from db_connection import get_connection
+
+conn = get_connection()
+
+if conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT DATABASE();")
+    result = cursor.fetchone()
+
+    print("Current Database:", result)
+
+    cursor.close()
+    conn.close()
